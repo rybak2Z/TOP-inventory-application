625a89f9acd3ee7c4c4dcb0fccca7732cc078221
Felix Fischer <felixfischer0204@gmail.com>
2023-10-04 20:06:22 +0200

    Add test data generation script

---
+++ populatedb.js
@@ -0,0 +1,125 @@
+#! /usr/bin/env node
+
+console.log(
+  'This script populates some test data to the database. Specify database URI and database name as "MONGODB_URI" and "DB_NAME" environment variables.',
+);
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+const Stick = require('./models/stick');
+const Category = require('./models/category');
+
+const categories = [];
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await connectToDb();
+  await createCategories(); // must come before sticks, bc sticks reference categories
+  await createSticks();
+  console.log('Closing mongoose...');
+  mongoose.connection.close();
+}
+
+async function connectToDb() {
+  mongoose.set('strictQuery', false);
+  const mongoDbUri = process.env.MONGODB_URI;
+  const databaseName = process.env.DB_NAME;
+  console.log('Connecting to database...');
+  await mongoose.connect(mongoDbUri, { dbName: databaseName });
+  console.log('Connected.');
+}
+
+async function createSingleStick(
+  name,
+  description,
+  category,
+  price,
+  has_image,
+) {
+  if (!category) console.log('scheise');
+  const stick = new Stick({
+    name,
+    description,
+    category,
+    price,
+    has_image,
+  });
+  await stick.save();
+  console.log(`Created stick ${name}`);
+}
+
+// Pass index to save in global array so that they can always be referenced
+// in predetermined order, no matter in what order the promises finish
+async function createSingleCategory(index, name) {
+  const category = new Category({ name });
+  await category.save();
+  categories[index] = category;
+  console.log(`Created category ${name}`);
+}
+
+async function createSticks() {
+  console.log('Creating sticks...');
+  await Promise.all([
+    createSingleStick(
+      'Stick gun #1',
+      'Gun description #1',
+      categories[0],
+      5,
+      false,
+    ),
+    createSingleStick(
+      'Stick gun #2',
+      'Gun description #2',
+      categories[0],
+      7,
+      false,
+    ),
+    createSingleStick(
+      'Stick rifle #1',
+      'Rifle description #1',
+      categories[1],
+      15,
+      false,
+    ),
+    createSingleStick(
+      'Stick rifle #2',
+      'Rifle description #2',
+      categories[1],
+      12,
+      false,
+    ),
+    createSingleStick(
+      'Stick sword #1',
+      'Sword description #1',
+      categories[2],
+      20,
+      false,
+    ),
+    createSingleStick(
+      'Stick dagger #1',
+      'Dagger description #1',
+      categories[3],
+      5,
+      false,
+    ),
+    createSingleStick(
+      'Stick other #1',
+      'Other description #1',
+      categories[4],
+      10,
+      false,
+    ),
+  ]);
+}
+
+async function createCategories() {
+  console.log('Creating categories...');
+  await Promise.all([
+    createSingleCategory(0, 'Guns'),
+    createSingleCategory(1, 'Rifles'),
+    createSingleCategory(2, 'Swords'),
+    createSingleCategory(3, 'Daggers'),
+    createSingleCategory(4, 'Other'),
+  ]);
+}
