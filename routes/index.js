91b077b64a1c390c2030ae1276aa4081d965c6a9
rybak2Z <rybak2Z.000@gmail.com>
2023-10-05 16:58:43 +0200

    Remove console log call

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,6 @@ router.get(
   '/',
   asyncHandler(async (req, res, next) => {
     const allSticks = await Stick.find({}).populate('category').exec();
-    console.log(allSticks);
     res.render('index', { sticks: allSticks });
   }),
 );
