5de3802f497095c1d2db995304f623dc8402947f
Felix Fischer <felixfischer0204@gmail.com>
2023-10-05 18:28:22 +0200

    Add validation to stick form

--- routes/stick.js
+++ routes/stick.js
@@ -1,9 +1,44 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
 const Stick = require('../models/stick');
 const Category = require('../models/category');
 
 const router = express.Router();
+
+function createStickFormValidationChains() {
+  return [
+    body('name')
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage('Name must not be empty.')
+      .escape()
+      .isLength({ max: 100 })
+      .withMessage('Name must be at most 100 characters long.')
+      .isAlphanumeric()
+      .withMessage('Name must only contain alphanumeric characters.'),
+    body('description')
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage('Description must not be empty.')
+      .escape()
+      .isLength({ max: 200 })
+      .withMessage('Description must be at most 200 characters long.')
+      .isAlphanumeric()
+      .withMessage('Description must only contain alphanumeric characters.'),
+    body('category').custom(async (categoryId) => {
+      const allCategories = await Category.find({}).select('_id').exec();
+      for (const category of allCategories) {
+        if (categoryId === category._id) {
+          return true;
+        }
+      }
+    }),
+    body('price', 'Price must be an integer greater or equal to 0.').isInt({
+      min: 0,
+    }),
+  ];
+}
 
 router.get(
   '/create',
@@ -15,6 +50,7 @@ router.get(
 
 router.post(
   '/create',
+  createStickFormValidationChains(),
   asyncHandler(async (req, res, next) => {
     const stick = new Stick({
       name: req.body.name,
@@ -23,6 +59,16 @@ router.post(
       price: req.body.price,
       has_image: false,
     });
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const allCategories = await Category.find({}).exec();
+      res.render('stickForm', {
+        categories: allCategories,
+        stick,
+        errors: errors.array(),
+      });
+    }
 
     await stick.save();
     res.redirect(stick.url);
@@ -53,6 +99,7 @@ router.get(
 
 router.post(
   '/:id/update',
+  createStickFormValidationChains(),
   asyncHandler(async (req, res, next) => {
     const stick = new Stick({
       name: req.body.name,
@@ -62,6 +109,16 @@ router.post(
       has_image: false,
       _id: req.params.id,
     });
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const allCategories = await Category.find({}).exec();
+      res.render('stickForm', {
+        categories: allCategories,
+        stick,
+        errors: errors.array(),
+      });
+    }
 
     const updatedStick = await Stick.findByIdAndUpdate(
       req.params.id,
